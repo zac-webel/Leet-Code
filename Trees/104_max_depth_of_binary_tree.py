9c149fcf14c8aaef66b86ce0432aecf7376c88ce
zac-webel <zow2@georgetown.edu>
2026-01-27 21:30:22 -0500

    Trees

---
+++ Trees/104_max_depth_of_binary_tree.py
@@ -0,0 +1,38 @@
+# Given the root of a binary tree, return its maximum depth.
+
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+# Example 2:
+
+# Input: root = [1,null,2]
+# Output: 2
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 104].
+# -100 <= Node.val <= 100
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+
+        # treat any valid node as value = 1
+        # run DFS recurively so when we backtrack we already have the left and right values 
+        # left and right equal the height if that node was the root
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left,right)
+        
---
+++ Trees/226_invert_binary_tree.py
@@ -0,0 +1,31 @@
+# Given the root of a binary tree, invert the tree, and return its root.
+
+# Input: root = [4,2,7,1,3,6,9]
+# Output: [4,7,2,9,6,3,1]
+
+# Input: root = [2,1,3]
+# Output: [2,3,1]
+
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        # swap 
+        root.left, root.right = root.right, root.left
+
+        # call recursively DFS. 
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
